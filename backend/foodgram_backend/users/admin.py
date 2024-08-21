0d7d731002aaffc4d353b66a9310fd598ef85d49
Ruslan Iskhakov <ruslan.iskhak94@gmail.com>
2024-08-21 16:02:09 +0300

    subscribing via admin fixed

--- backend/foodgram_backend/users/admin.py
+++ backend/foodgram_backend/users/admin.py
@@ -1,4 +1,4 @@
-from django.contrib import admin
+from django.contrib import admin, messages
 from django.contrib.auth.admin import UserAdmin
 
 from .models import Subscribe, User
@@ -8,9 +8,20 @@ class FoodgramAdmin(UserAdmin):
     search_fields = ('username', 'email')
 
 
+class SubscribeAdmin(admin.ModelAdmin):
+
+    def save_model(self, request, obj, form, change):
+        if obj.user == obj.subscribing:
+            messages.set_level(request, messages.ERROR)
+            message = 'User cannot subscribe on himself!!'
+            self.message_user(request, message, level=messages.ERROR)
+        else:
+            obj.save()
+
+
 FoodgramAdmin.fieldsets += (
     ('Extra Fields', {'fields': ('avatar',)}),
 )
 
 admin.site.register(User, FoodgramAdmin)
-admin.site.register(Subscribe)
+admin.site.register(Subscribe, SubscribeAdmin)
